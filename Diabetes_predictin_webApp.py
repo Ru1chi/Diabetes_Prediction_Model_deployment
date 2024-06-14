a426487095877624ced434d5f659c623a92861a3
Ru1chi <116619582+Ru1chi@users.noreply.github.com>
2024-06-14 12:07:13 +0530

    Update Diabetes_predictin_webApp.py

--- Diabetes_predictin_webApp.py
+++ Diabetes_predictin_webApp.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model=pickle.load(open('trained_model.sav','rb'))#load function is used to load the saved model
+loaded_model=pickle.load(open(r'trained_model.sav','rb'))#load function is used to load the saved model
 
 
 #creating a function for prediction
